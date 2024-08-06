4dc7bd31744060eb2b38805def72d8d10831f5a2
gloomikon <gloomikon@gmail.com>
2024-08-06 14:17:55 +0200

    finish chapter 2

---
+++ 02-nsmanagedobject-subclasses/projects/starter/BowTies/BowTie+CoreDataClass.swift
@@ -0,0 +1,7 @@
+import Foundation
+import CoreData
+
+@objc(BowTie)
+public class BowTie: NSManagedObject {
+
+}
--- 02-nsmanagedobject-subclasses/projects/starter/BowTies/BowTies/AppDelegate.swift
+++ 02-nsmanagedobject-subclasses/projects/starter/BowTies/BowTies/AppDelegate.swift
@@ -37,11 +37,11 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
 
-  func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    true
+  func application(_ application: UIApplication,
+                    didFinishLaunchingWithOptions
+    launchOptions: [UIApplication.LaunchOptionsKey: Any]?)
+    -> Bool {
+    return true
   }
 
   // MARK: UISceneSession Lifecycle
